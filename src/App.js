4af367aa49b796374bb8e006709008ca9e68b2fa
Shovan Banik <53087831+Shovan-Banik@users.noreply.github.com>
2023-03-23 12:53:14 +0600

    4 core concept practice done

--- src/App.js
+++ src/App.js
@@ -2,24 +2,34 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const articlePara={
+    textAlign:'center',
+    textShadow:'1px 1px',
+    fontStyle:'italic',
+    font:'bold'
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+      <article style={{backgroundColor:'yellow',border:'4px solid black',margin:'10px',borderRadius:'10px',padding:'20px'}}>
+        <h2 className='blog'>My failure life</h2>
+        <p style={articlePara}>
+        Ok, i will write about my self. I  am a failure still today. Still now i want money from my father. It's to much painful. It is time to support my parents but i beg support from parents still now. Sorry parents for my all faults. I love both of you more than my life.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </article>
+      <Blog name='Shovan' home='Balla'></Blog>
+      <Blog name='Raja' home='Balla'></Blog>
+      <Blog name='Proja' home='Mirpur-1'></Blog>
     </div>
   );
 }
+function Blog(props){
+  return(
+    <div>
+      <h2>Failure person:{props.name}</h2>
+      <p>Home:{props.home}</p>
+    </div>
+  )
+}
+
 
 export default App;
